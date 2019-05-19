9ddac225088334840c2b52cb3b438052dc379c3a
Grewer <grewer@grewer.cn>
2019-05-19 21:22:54 +0800

    图像识别更清晰 方法

--- imgtest.py
+++ imgtest.py
@@ -2,23 +2,65 @@
 import execjs  # 必须，需要先用pip 安装，用来执行js脚本
 from bs4 import BeautifulSoup
 import requests
-from PIL import Image
+from PIL import Image,ImageEnhance,ImageFilter
 import json
 import urllib
 import pytesseract
-# import sys
-# reload(sys)
-# sys.setdefaultencoding('utf8')
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030') 
+import sys  
+
+
+def image_to_string(image):
+    return pytesseract.image_to_string(image, lang='jpn')
+print('run start')
+
+# 二值化  
+threshold = 140  
+table = []  
+for i in range(256):  
+    if i < threshold:  
+        table.append(0)  
+    else:  
+        table.append(1)  
+  
+def  getverify1(name):        
+    #打开图片  
+	im = Image.open(name)  
+    #转化到灰度图
+	imgry = im.convert('L')
+    #保存图像
+	imgry.save(name)  
+    #二值化，采用阈值分割法，threshold为分割点 
+	out = imgry.point(table,'1')  
+	out.save(name)  
+    #识别  
+	text = image_to_string(out)  
+    #识别对吗  
+	text = text.strip()  
+	text = text.upper()
+	return text  
+text = getverify1('C:\\Users/Admin/Desktop/1.PNG')
+
+print('text start')
+print(text)
+print('text end')
 
 # if pytesseract env is none
 # pytesseract.pytesseract.tesseract_cmd = '/usr/local/Cellar/tesseract/4.0.0_1/bin/tesseract'
 # text = pytesseract.image_to_string(Image.open('/Users/admin/grewer/py/testEng.png'), lang='eng')
 # text = pytesseract.image_to_string(Image.open('/Users/admin/grewer/py/testEng.png'), lang='eng')
-text = pytesseract.image_to_string(Image.open(
-    '/Users/admin/grewer/py/testJpn.jpg'), lang='jpn')
+# text = pytesseract.image_to_string(Image.open('C:\\Users/Admin/Desktop/buhuo.PNG'), lang='jpn')
+# text = pytesseract.image_to_string(Image.open('C:\\Users/Admin/Desktop/1.PNG'), lang='jpn')
+# text = pytesseract.image_to_string(Image.open('C:\\Users/Admin/Desktop/testJpn.jpg'), lang='jpn')
 
 
-print(text)
+
+
+# print('text start')
+# print(text)
+# print('text end')
 
 # 有道
 # req_url = 'http://fanyi.youdao.com/translate'  # 创建连接接口
@@ -42,40 +84,40 @@ class Py4Js():
 		self.ctx = execjs.compile("""
 		function TL(a) {
 		var k = "";
-	    var b = 406644;
-	    var b1 = 3293161072;
-	    var jd = ".";
-	    var $b = "+-a^+6";
-	    var Zb = "+-3^+b+-f";
-	    for (var e = [], f = 0, g = 0; g < a.length; g++) {
-	        var m = a.charCodeAt(g);
-	        128 > m ? e[f++] = m : (2048 > m ? e[f++] = m >> 6 | 192 : (55296 == (m & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (m = 65536 + ((m & 1023) << 10) + (a.charCodeAt(++g) & 1023),
-	        e[f++] = m >> 18 | 240,
-	        e[f++] = m >> 12 & 63 | 128) : e[f++] = m >> 12 | 224,
-	        e[f++] = m >> 6 & 63 | 128),
-	        e[f++] = m & 63 | 128)
-	    }
-	    a = b;
-	    for (f = 0; f < e.length; f++) a += e[f],
-	    a = RL(a, $b);
-	    a = RL(a, Zb);
-	    a ^= b1 || 0;
-	    0 > a && (a = (a & 2147483647) + 2147483648);
-	    a %= 1E6;
-	    return a.toString() + jd + (a ^ b)
+		var b = 406644;
+		var b1 = 3293161072;
+		var jd = ".";
+		var $b = "+-a^+6";
+		var Zb = "+-3^+b+-f";
+		for (var e = [], f = 0, g = 0; g < a.length; g++) {
+			var m = a.charCodeAt(g);
+			128 > m ? e[f++] = m : (2048 > m ? e[f++] = m >> 6 | 192 : (55296 == (m & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (m = 65536 + ((m & 1023) << 10) + (a.charCodeAt(++g) & 1023),
+			e[f++] = m >> 18 | 240,
+			e[f++] = m >> 12 & 63 | 128) : e[f++] = m >> 12 | 224,
+			e[f++] = m >> 6 & 63 | 128),
+			e[f++] = m & 63 | 128)
+		}
+		a = b;
+		for (f = 0; f < e.length; f++) a += e[f],
+		a = RL(a, $b);
+		a = RL(a, Zb);
+		a ^= b1 || 0;
+		0 > a && (a = (a & 2147483647) + 2147483648);
+		a %= 1E6;
+		return a.toString() + jd + (a ^ b)
 	  };
 	  function RL(a, b) {
-	    var t = "a";
-	    var Yb = "+";
-	    for (var c = 0; c < b.length - 2; c += 3) {
-	        var d = b.charAt(c + 2),
-	        d = d >= t ? d.charCodeAt(0) - 87 : Number(d),
-	        d = b.charAt(c + 1) == Yb ? a >>> d: a << d;
-	        a = b.charAt(c) == Yb ? a + d & 4294967295 : a ^ d
-	    }
-	    return a
+		var t = "a";
+		var Yb = "+";
+		for (var c = 0; c < b.length - 2; c += 3) {
+			var d = b.charAt(c + 2),
+			d = d >= t ? d.charCodeAt(0) - 87 : Number(d),
+			d = b.charAt(c + 1) == Yb ? a >>> d: a << d;
+			a = b.charAt(c) == Yb ? a + d & 4294967295 : a ^ d
+		}
+		return a
 	  }
- 	""")
+	 """)
 
 	def getTk(self, text):
 		return self.ctx.call("TL", text)
